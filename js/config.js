a7f4a61b93ba7f436b439dd75e453b1a3c478da2
unknown <god_lm@yeah.net>
2017-07-11 16:10:31 +0800

    添加文件

---
+++ js/config.js
@@ -0,0 +1,25 @@
+﻿var app = {
+  teams: [
+    { name: '朱润亚', tel: '18610279719', email: 'zry@jusfoun.com', qq: '86427955', des: '猥琐大叔，嘿嘿', url: '/users/zhurunya',group:'*' },
+    { name: '魏彬', tel: '13401095555', email: 'weibin@jusfoun.com', qq: '23004532', des: '懒而不惰~', url: '/users/weibin',group:'pro2' },
+    { name: '邵金东', tel: '15378789898', email: 'sjd@jusfoun.com', qq: '81288819', des: '....', url: '/users/shaojindong' ,group:'pro2'},
+    { name: '柳杨', tel: '18601265717', email: 'ly@jusfoun.com', qq: '451870307', des: '没有强迫症的处女座', url: '/users/yangliu' ,group:'pro2'},
+    { name: '陈胜', tel: '18602685946', email: 'chensheng@jusfoun.com', qq: '809966184', des: '....', url: '/users/chensheng' ,group:'pro2'},
+    { name: '王帅', tel: '15313163637', email: 'ws@jusfoun.com', qq: '188009510', des: '....', url: '/users/wangshuai' ,group:'pro1'},
+    { name: '邢玮', tel: '18301150703', email: 'xw@jusfoun.com', qq: '1065641709', des: '....', url: '/users/xingwei' ,group:'pro1'},
+    { name: '杨勇冠', tel: '17600801597', email: 'yyg@jusfoun.com', qq: '1135137781', des: '自律者自强 ...', url: '/users/yangyongguan' ,group:'pro3'},
+    { name: '王杰', tel: '15001316150', email: 'wangjie@jusfoun.com', qq: '   674947294', des: '程序员', url: '/users/wangjie' ,group:'pro3'},
+    { name: '杨微', tel: '18519292486', email: 'yangwei@jusfoun.com', qq: '873483161', des: '不一样色儿的呲花', url: '/users/yangwei' ,group:'pro3'},
+    { name: '商业庆', tel: '13121730259', email: 'shangyeqing@jusfoun.com', qq: '1246283740', des: '....', url: '/users/shangyeqing' ,group:'pro1'},
+    { name: '魏阁', tel: '18310634607', email: 'wg@jusfoun.com', qq: '3129168699', des: '努力 拼搏', url: '/users/weige' ,group:'pro1'},
+    { name: '杨杰', tel: '17801129475', email: 'yangjie@jusfoun.com', qq: '624699461', des: '....', url: '/users/yangjie' ,group:'pro1'},
+    { name: '李猛', tel: '18618133836', email: 'limeng@jusfoun.com', qq: '1526437167', des: '做好自己', url: '/users/limeng' ,group:'pub'},
+    { name: '汝银娟', tel: '18611833241', email: 'ryj@jusfoun.com', qq: '1601450116', des: '90后女汉子', url: '/users/ruyinjuan'  ,group:'pub'},
+    { name: '郭惠敏', tel: '15810169916', email: 'ghm@jusfoun.com', qq: '2777061859', des: '小萝莉', url: '/users/guohuimin'  ,group:'pub'},
+    { name: '冯彦文', tel: '15201565398', email: 'fyw@jusfoun.com', qq: '793268593', des: '....', url: '/users/fengyanwen'  ,group:'pub'},
+    { name: '任新杰', tel: '15611346676', email: 'rxj@jusfoun.com', qq: '346494891', des: '....', url: '/users/renxinjie' ,group:'pro4'},
+    { name: '颜庭光', tel: '15201147857', email: 'ytg@jusfoun.com', qq: '522125842', des: '....', url: '/users/yantingguang'  ,group:'pro4'},
+    { name: '韩凯波', tel: '18610815241', email: 'hkb@jusfoun.com', qq: '1342233762', des: '....', url: '/users/hankaibo'  ,group:'pro4'},
+    { name: '吕颖萍', tel: '15201147857', email: 'lvyingping@jusfoun.com', qq: '383418784', des: '....', url: '/users/lvyingping'  ,group:'pro4'}
+    ]
+};
