fe666d42dd225da07d5b8ed189f1061a44605427
zhu18 <zhu18@vip.qq.com>
2017-07-13 16:57:59 +0800

    ..

--- js/custom_min.js
+++ js/custom_min.js
@@ -59,6 +59,7 @@ function vaildateNet(){
 }
 
 $(function () {
+    bannerBG();
     var b = $("#filter_container");
     b.isotope({itemSelector: ".element"});
     var a = $("#filter_header .option-set"), c = a.find("a");
@@ -90,3 +91,100 @@ $(function () {
         return false
     })
 });
+
+
+function bannerBG(){
+    particlesJS("rank-banner", {
+        particles: {
+            number: {
+                value: 40,
+                density: {
+                    enable: !0,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 0,
+                    color: "#000000"
+                },
+                polygon: {
+                    nb_sides: 5
+                },
+                image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100
+                }
+            },
+            opacity: {
+                value: .3,
+                random: !1,
+                anim: {
+                    enable: !1,
+                    speed: 1,
+                    opacity_min: .1,
+                    sync: !1
+                }
+            },
+            size: {
+                value: 20,
+                random: !0,
+                anim: {
+                    enable: !1,
+                    speed: 30,
+                    size_min: .1,
+                    sync: !1
+                }
+            },
+            line_linked: {
+                enable: !0,
+                distance: 150,
+                color: "#ffffff",
+                opacity: .2,
+                width: 1
+            },
+            move: {
+                enable: !0,
+                speed: 4,
+                direction: "none",
+                random: !0,
+                straight: !1,
+                out_mode: "out",
+                bounce: !1,
+                attract: {
+                    enable: !1,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: !1,
+                    mode: "grab"
+                },
+                onclick: {
+                    enable: !1,
+                    mode: "push"
+                },
+                resize: !0
+            },
+            modes: {
+                grab: {
+                    distance: 140,
+                    line_linked: {
+                        opacity: 1
+                    }
+                }
+            }
+        },
+        retina_detect: !0
+    });
+}
