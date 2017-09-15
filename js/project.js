180659b641bc3e814c97bd550ba6aad4ad3c5579
huiminguo <guohuiminit@sina.com>
2017-09-15 13:45:12 +0800

    前端项目

---
+++ js/project.js
@@ -0,0 +1,39 @@
+/**
+ * Created by guohuimin on 2017/9/8.
+ */
+$(document).ready(function () {
+
+    $.ajax({
+        url: "project/9cf.md",
+        success: function (data) {
+
+            var $node = $(marked(data));
+            $("li",$node[0]).each(function (index,item) {
+                var $node = $(item);
+                var imgPath = $("img",$node).attr("src");
+                $("#con" + index%4).append($node);
+                $(".markdown-content-body li p a").attr("target","_blank");
+                $("img",$node).attr("data-original",imgPath).attr("src","img/loading.gif");
+                $(".markdown-content-body li").click(function(){
+                    var href = $(this).find("a").attr("href");
+                    window.open(href);
+                });
+            });
+
+            $("#con0 img").lazyload({
+                effect: "fadeIn"
+            });
+            $("#con1 img").lazyload({
+                effect: "fadeIn"
+            });
+            $("#con2 img").lazyload({
+                effect: "fadeIn"
+            });
+            $("#con3 img").lazyload({
+                effect: "fadeIn"
+            });
+        }
+    })
+});
+
+
