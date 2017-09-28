fa07c43a37356445796d32242b7175c5cf6073e7
huiminguo <guohuiminit@sina.com>
2017-09-28 18:52:30 +0800

    首页、前端项目修改

--- js/custom_min.js
+++ js/custom_min.js
@@ -31,10 +31,10 @@ $(document).ready(function () {
     });*/
     $("span.mask").hover(function () {
         $(this).siblings("a img").addClass("hovering");
-        $(this).parent().siblings(".portfolio-title").children("h4").stop().animate({top: -20}, 350)
+        $(this).parent().siblings(".portfolio-title").css({"height":"40px"}).children("h4").stop().animate({top: -20}, 350)
     }, function () {
         $(this).siblings("a img").removeClass("hovering");
-        $(this).parent().siblings(".portfolio-title").children("h4").stop().animate({top: 0}, 350)
+        $(this).parent().siblings(".portfolio-title").css({"height":"20px"}).children("h4").stop().animate({top: 0}, 350)
     });
     $("a[href*=#]:not([href=#])").click(function () {
         if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") || location.hostname == this.hostname) {
--- js/project.js
+++ js/project.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
         url: "project/9cf.md",
         success: function (data) {
 
-            var $node = $(marked(data));
+            var $node = $(marked(data)),$item;
             $("li",$node[0]).each(function (index,item) {
-                var $node = $(item);
-                var $img = $("img",$node);
+                $item = $(item);
+                var $img = $("img:eq(0)",$item);
                 var imgPath = $img.attr("src");
-                $img.attr("src",'');
-                $("img",$node).attr("data-original",imgPath);
-                $("#con" + index%4).append($node);
+                $img.attr({"src":'',"data-original":imgPath});
+                $("img:eq(1)",$item).attr("tag",1);
+                $("#con" + index%4).append($item);
             });
-            $(".projects-markdown li p a").attr("target","_blank");
+            $(".projects-markdown li a").attr("target","_blank");
             $(".projects-markdown li").click(function(){
                 var href = $(this).find("a").attr("href");
                 window.open(href);
@@ -26,19 +26,12 @@ $(document).ready(function () {
                     $(this).addClass("img-loding");
                 })
 
+            $("img[tag=1]").click(function () {
+                var href = $(this).parent("a").attr("href")
+                window.open(href);
+                return false;
+                })
             })
-            // $("#projectsRow img").lazyload({
-            //     effect: "fadeIn"
-            // });
-            // $("#con1 img").lazyload({
-            //     effect: "fadeIn"
-            // });
-            // $("#con2 img").lazyload({
-            //     effect: "fadeIn"
-            // });
-            // $("#con3 img").lazyload({
-            //     effect: "fadeIn"
-            // });
         }
     })
 });
