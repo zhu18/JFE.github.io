31a8db5df6e46ebfb312791ef34afd156d0ada77
huiminguo <guohuiminit@sina.com>
2017-10-12 18:27:42 +0800

    change

--- js/project.js
+++ js/project.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
                 $("#con" + index%4).append($item);
                 //使用技术
                 var $div = $("div:eq(0)",$item);
-                console.log($div);
+                //console.log($div);
                 var technology=$div.html().split(',');
                 $div.addClass("div-technology").html("");
                 $div.append("<span style='font-size:13px;'>使用技术：</span>");
@@ -37,28 +37,34 @@ $(document).ready(function () {
                     var icon = icons[item];
                     $div.append($("<i>").attr("class","icon iconfont " + icon).attr("title",item));
                 });
-
             });
             $(".projects-markdown li a").attr("target","_blank");
             $(".projects-markdown li").click(function(){
                 var href = $(this).find("a").attr("href");
                 window.open(href);
             });
+
+            $(".projects-markdown li").each(function (index,item) {
+                var i = $("<i class='icon iconfont icon-github1' />").css({"float":"right"});
+                var a_url = $("p:eq(0) a",$(this)).attr("href");
+                $(item).append(i);
+                var strArray = a_url.split("/")
+                var s1 = strArray[2].split(".")[0];
+                var s2 = strArray[3];
+                $(".icon-github1",$(this)).attr("href","https://github.com/"+s1+"/"+s2);
+                //跳转到git页面
+                $(".icon-github1",$(this)).click(function (){
+                    var href = $(this).attr("href")
+                    window.open(href);
+                    return false;
+                })
+            })
+
             $("#projectsRow img").scrollLoad(function () {
                 $(this).attr("src",$(this).attr("data-original")).on("load",function(){
                     $(this).addClass("img-loding");
                 })
-
-            $("img[tag=1]").click(function () {
-                var href = $(this).parent("a").attr("href")
-                window.open(href);
-                return false;
-                })
-            // 所用技术
-
-
             })
-            /*  */
         }
     })
 });
